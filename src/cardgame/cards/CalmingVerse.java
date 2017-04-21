f79f5a2bafd50f81006d7afe6e2d5df314f48a74
Unknown <buzdugan.1996@mail.ru>
2017-04-21 16:00:07 +0200

    Aggiunta Calming Verse

---
+++ src/cardgame/cards/CalmingVerse.java
@@ -0,0 +1,47 @@
+
+package cardgame.cards;
+
+import cardgame.AbstractCardEffect;
+import java.util.ArrayList;
+import cardgame.Card;
+import cardgame.Effect;
+import cardgame.Player;
+import cardgame.CardGame;
+import cardgame.Enchantment;
+import java.util.Scanner;
+
+public class CalmingVerse implements Card {
+    private class CalmingVerseEffect extends AbstractCardEffect {
+        Player to;
+        public CalmingVerseEffect(Player p, Card c, Player targetOwner) {
+            super(p,c);
+            to = targetOwner;
+        }
+        
+        @Override
+        public void resolve () {
+            to.getEnchantments().clear();
+        }
+    }
+   
+
+    @Override
+    public Effect getEffect(Player p) {
+        return new CalmingVerseEffect(p,this,CardGame.instance.getCurrentAdversary());
+    }
+    
+    
+    
+    @Override
+    public String name() { return "CalmingVerse"; }
+    @Override
+    public String type() { return "Enchantment"; }
+    @Override
+    public String ruleText() { return "Destroys all enchantments you don't control"; }
+    @Override
+    public String toString() { return name() + " (" + type() + ") [" + ruleText() +"]";}
+    @Override
+    public boolean isInstant() { return false; }
+    
+
+}
