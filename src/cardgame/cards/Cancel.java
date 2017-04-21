a2873f4fddb1686cafbf6c18e7c32bddf3e3a291
Roberto <r.p.computer2@gmail.com>
2017-04-21 15:06:45 +0200

    Correzione cancel

--- src/cardgame/cards/Cancel.java
+++ src/cardgame/cards/Cancel.java
@@ -1,91 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package cardgame.cards;
 
+import cardgame.AbstractCardEffect;
 import java.util.ArrayList;
 import cardgame.Card;
 import cardgame.Effect;
-import cardgame.Enchantment;
 import cardgame.Player;
-import cardgame.AbstractEnchantmentCardEffect;
-import cardgame.AbstractEnchantment;
 import cardgame.CardGame;
-import cardgame.Creature;
-import cardgame.TriggerAction;
-import cardgame.Triggers;
 import java.util.Scanner;
 
-/**
- *
- * @author atorsell
- */
 public class Cancel implements Card {
-    private class CancelEffect extends AbstractEnchantmentCardEffect {
-    
+    private class CancelEffect extends AbstractCardEffect {
+        Effect target = null;
         
-        public CancelEffect(Player p, Card c) { super(p,c); }
-        @Override
-        protected Enchantment createEnchantment() { return new CancelEnchantment(owner); }
+        public CancelEffect(Player p, Card c, Effect t) { super(p,c); target = t;}
         
         @Override
         public void resolve () {
-            ArrayList <Effect> l = new ArrayList<>();
-            int i=0;
-      
-            for (Effect c: CardGame.instance.getStack()){
-                l.add(c);                                 
-            }    
-            
-            System.out.println("Choose an effect to cancel");
-            for (Effect e: l){
-                System.out.println(i+""+e.toString()); // da cambiare getString in getCardName
-                i++;
-            }  
-            
-            Scanner s = new Scanner (System.in);
-            i=s.nextInt();
-            CardGame.instance.getStack().remove(l.get(i));
-        
-
+            CardGame.instance.getStack().remove(target);
         }
     }
    
 
     @Override
-    public Effect getEffect(Player p) { return new CancelEffect(p,this); }
-    
-    private class CancelEnchantment extends AbstractEnchantment {
-        public CancelEnchantment(Player owner) {
-            super(owner);
-        }
-        
-        private final TriggerAction GreetingAction = new TriggerAction() {
-                @Override
-                public void execute(Object args) {
-                    if (args != null  && args instanceof Creature) {
-                        Creature c = (Creature)args;
-                        System.out.println( name() + " says: \"Welcome " + c.name() +"!\"" );
-                    }
-                }
-            };
-        
-        @Override
-        public void insert() {
-            super.insert();
-            CardGame.instance.getTriggers().register(Triggers.ENTER_CREATURE_FILTER, GreetingAction);
-        }
-        
-        public void removeEffect() { // ho cambiato remove() in removeEffect()  
-            super.remove();
-            CardGame.instance.getTriggers().deregister(GreetingAction);
-        }
-        
-        @Override
-        public String name() { return "Boiling Earth"; }
+    public Effect getEffect(Player p) {
+        ArrayList <Effect> l = new ArrayList<>();
+        int i=0;
+
+        for (Effect c: CardGame.instance.getStack()){
+            l.add(c);                                 
+        }    
+
+        System.out.println("Choose an effect to cancel");
+        for (Effect e: l){
+            System.out.println(i+") " + e.toString()); // da cambiare getString in getCardName
+            i++;
+        }  
+
+        Scanner s = new Scanner (System.in);
+        i=s.nextInt();
+        Effect e = l.get(i);
+        return (Effect) new CancelEffect(p, this, e);
     }
+    
     
     
     @Override
